b030277b1ead14481a41fee076cac5027fff5357
Guilhem Lettron <guilhem@lettron.fr>
2013-04-29 13:13:37 +0200

    upgrade solr to 4.2.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default["solr_app"]["download_site"]	= "http://apache.crihan.fr/dist/lucene/solr"
-default["solr_app"]["version"]		= "4.1.0"
+default["solr_app"]["version"]		= "4.2.1"
 default["solr_app"]["url"]		= [ node["solr_app"]["download_site"], node["solr_app"]["version"], "solr-#{node["solr_app"]["version"]}.tgz" ].join("/")
 
 default["solr_app"]["archive_war_path"]	= File.join("solr-#{node["solr_app"]["version"]}", "dist", "solr-#{node["solr_app"]["version"]}.war")
