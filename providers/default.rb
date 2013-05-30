427ae859c3d6040e51179efa95d72252f6f4cc4b
Guilhem Lettron <guilhem@lettron.fr>
2013-05-30 10:31:57 +0200

    cleanup provider

--- providers/default.rb
+++ providers/default.rb
@@ -1,8 +1,8 @@
 action :create do
   remote_directory "Solr collection" do
-    path "#{node["solr_app"]["solr_home"]}/#{new_resource.name}"
+    path ::File.join(node["solr_app"]["solr_home"], new_resource.name)
     source new_resource.directory || new_resource.name
-    if new_resource.cookbook then cookbook new_resource.cookbook end
+    cookbook new_resource.cookbook if new_resource.cookbook
     owner node["tomcat"]["user"]
     group node["tomcat"]["group"]
     action :create
